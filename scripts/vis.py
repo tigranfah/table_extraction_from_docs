725e6b86743dde3eece59436b84bc7277be2e51b
Tigran Fahradyan <tigran.fahradyan01@gmail.com>
2022-07-22 17:18:09 +0400

    asd

--- scripts/vis.py
+++ scripts/vis.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 
 
-def imshow(image, fac=50):
+def imshow(image, fac=50, mi=0, ma=1):
     fig = plt.figure(figsize=(image.shape[0]/fac, image.shape[1]/fac))
     ax = fig.add_subplot()
-    ax.imshow(image)
+    ax.imshow(image, cmap="gray")
     ax.set_xticks([])
     ax.set_yticks([])
 
 
 def anshow(image, mask, fac=15, mi=0, ma=1):
     fig, ax = plt.subplots(1, 2, figsize=(image.shape[0]/fac, image.shape[1]/fac))
-    ax[0].imshow(image, cmap="gray", vmin=mi, vmax=1)
+    ax[0].imshow(image, cmap="gray", vmin=mi, vmax=ma)
     ax[0].set_xticks([])
     ax[0].set_yticks([])
 
-    ax[1].imshow(mask, cmap="gray", vmin=mi, vmax=1)
+    ax[1].imshow(mask, cmap="gray", vmin=mi, vmax=ma)
     ax[1].set_xticks([])
     ax[1].set_yticks([])
     plt.show()
